4122647e7b7e28e12133f50ded16c8945682e2d5
AhmadSaleh99 <saleehsale816@gmail.com>
2023-01-11 19:20:12 +0530

    s

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Stack, Typography, Button } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import HeroBannerImage from "../assets/images/banner.png";
 
 const HeroBanner = () => {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { Box } from "@mui/material";
 import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
